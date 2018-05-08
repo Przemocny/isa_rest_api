6f791e40d1347d5a90d6d74caaad85a4ecf02466
Przemysław <przemyslaw@devspirit.local>
2018-05-08 12:15:54 +0200

    table for assigned tasks

--- src/views/TasksView/components.js
+++ src/views/TasksView/components.js
@@ -3,11 +3,32 @@ import React, {Component} from 'react'
 const TasksAssingedToMe = ({data}) => {
   return (
     <div>
-      <ul>
+
+      <table>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>status</th>
+            <th>creator</th>
+            <th>assignee</th>
+            <th>hours</th>
+            <th>actions</th>
+          </tr>
+        </thead>
+        <tbody>
+
         {data.map((task, key) => {
-          return <li key={key}>{JSON.stringify(task)}</li>
-        })}
-      </ul>
+            return ( <tr key={key}>
+                <td>{task.title}</td>
+                <td>{task.status}</td>
+                <td>{task.creator}</td>
+                <td>{task.assignee}</td>
+                <td>{JSON.stringify(task.hours)}</td>
+                <td><button>akcja</button></td>
+              </tr>)
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
