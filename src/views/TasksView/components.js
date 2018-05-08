ebbfcd1930ac1e7adb1f6ed38453e3e2773421f0
Przemysław <przemyslaw@devspirit.local>
2018-05-08 13:34:05 +0200

    button for refresh

--- src/views/TasksView/components.js
+++ src/views/TasksView/components.js
@@ -4,7 +4,7 @@ const TasksAssingedToMe = ({data}) => {
   return (
     <div>
 
-      <table>
+      <table className="table table-striped">
         <thead>
           <tr>
             <th>title</th>
@@ -16,15 +16,19 @@ const TasksAssingedToMe = ({data}) => {
           </tr>
         </thead>
         <tbody>
-
         {data.map((task, key) => {
-            return ( <tr key={key}>
+            return (<tr key={key}>
                 <td>{task.title}</td>
                 <td>{task.status}</td>
                 <td>{task.creator}</td>
                 <td>{task.assignee}</td>
                 <td>{JSON.stringify(task.hours)}</td>
-                <td><button>akcja</button></td>
+                <td>
+                <button className="btn btn-default">Przypisz taska</button>
+                <button className="btn btn-default">Zmień status</button>
+                <button className="btn btn-default">Zmień opis</button>
+                <button className="btn btn-default">Zaloguj godziny</button>
+                </td>
               </tr>)
           })}
         </tbody>
--- src/views/TasksView/index.js
+++ src/views/TasksView/index.js
@@ -1,70 +1,100 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 
 // importujemy metody z api
-import {getAllTasksByMyEmail, getAllTaskForMe} from '../../api/tasks.api';
-
+import {getAllTasksByMyEmail, getAllTaskForMe} from '../../api/tasks.api'
 
 // importujemy komponenty składowe
 import {TasksAssingedToMe} from './components'
 
-
-
 class TasksView extends Component {
-    // deklaracja stanu komponentu
-    state = {
-        imBusy:true,
-        tasksAssignedToMe:[],
-        tasksCreatedByMe:[]
-    }
-    // metoda cyklu życia komponentu
-  componentDidMount(){
+  // deklaracja stanu komponentu
+  state = {
+    imBusy: true,
+    tasksAssignedToMe: [],
+    tasksCreatedByMe: []
+  }
+  // metoda cyklu życia komponentu
 
-    // promise.all([prom1,prom2,...]) -> wykonaj wszystkie promisy a potem zrób .then
-    Promise.all([
-        getAllTasksByMyEmail('Przemek'),
-        getAllTaskForMe('Przemek')
-    ]).then((data)=>{
-
-        const [tasksCreatedByMe, tasksAssignedToMe] = data;
-
-        // const tasksCreatedByMe = data[0];
-        // const tasksAssignedToMe = data[1];
-
-        console.log('tasks data', tasksCreatedByMe, tasksAssignedToMe);
-
-        this.setState({
-            tasksCreatedByMe, 
-            tasksAssignedToMe,
-            imBusy:false
-        })
-
-        // this.setState({
-        //     tasksCreatedByMe:tasksCreatedByMe,
-        //     tasksAssignedToMe:tasksAssignedToMe
-        // })
-
-       
-        // handle data
+  refresh () {
+    this.setState({
+      imBusy: true
     })
 
+    Promise.all([
+      getAllTasksByMyEmail('Przemek'),
+      getAllTaskForMe('Przemek')
+    ]).then(data => {
+      const [tasksCreatedByMe, tasksAssignedToMe] = data
+
+      // const tasksCreatedByMe = data[0];
+      // const tasksAssignedToMe = data[1];
+
+      console.log('tasks data', tasksCreatedByMe, tasksAssignedToMe)
+
+      this.setState({
+        tasksCreatedByMe,
+        tasksAssignedToMe,
+        imBusy: false
+      })
+
+      // this.setState({
+      //     tasksCreatedByMe:tasksCreatedByMe,
+      //     tasksAssignedToMe:tasksAssignedToMe
+      // })
+
+      // handle data
+    })
   }
 
-  render() {
+  componentDidMount () {
+    // promise.all([prom1,prom2,...]) -> wykonaj wszystkie promisy a potem zrób .then
+    Promise.all([
+      getAllTasksByMyEmail('Przemek'),
+      getAllTaskForMe('Przemek')
+    ]).then(data => {
+      const [tasksCreatedByMe, tasksAssignedToMe] = data
 
-    console.log('this.state',this.state)
-    if(this.state.imBusy){
-        return (
-            <span>JESTEM ZAJĘTY</span>
-        )
+      // const tasksCreatedByMe = data[0];
+      // const tasksAssignedToMe = data[1];
+
+      console.log('tasks data', tasksCreatedByMe, tasksAssignedToMe)
+
+      this.setState({
+        tasksCreatedByMe,
+        tasksAssignedToMe,
+        imBusy: false
+      })
+
+      // this.setState({
+      //     tasksCreatedByMe:tasksCreatedByMe,
+      //     tasksAssignedToMe:tasksAssignedToMe
+      // })
+
+      // handle data
+    })
+  }
+
+  render () {
+    console.log('this.state', this.state)
+    if (this.state.imBusy) {
+      return <span>JESTEM ZAJĘTY</span>
     }
 
     return (
-        <div>
-            <h2>TasksView</h2>
-            <TasksAssingedToMe {...this.state.tasksAssignedToMe}/>
-        </div>
-    );
+      <div>
+        <h2>TasksView</h2>
+        <button
+          className='btn btn-block btn-warning'
+          onClick={() => {
+            this.refresh()
+          }}
+        >
+          Odśwież
+        </button>
+        <TasksAssingedToMe {...this.state.tasksAssignedToMe} />
+      </div>
+    )
   }
 }
 
-export default TasksView;
+export default TasksView
